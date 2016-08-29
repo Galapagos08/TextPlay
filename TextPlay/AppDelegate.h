b2ee03b9c4fe6692c9047f8061690794a692fca2
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-29 17:06:12 -0400

    Initial Commit

---
+++ TextPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextPlay
+//
+//  Created by Dan Esrey on 2016/29/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextPlay
+//
+//  Created by Dan Esrey on 2016/29/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
